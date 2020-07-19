ace9e4eb7075178261798b7db409a708dbbad7d5
vietanhbuitran <anhbtvse06022@fpt.edu.vn>
2020-07-20 02:04:09 +0700

    update address information

--- src/views/UserInformationAddress.vue
+++ src/views/UserInformationAddress.vue
@@ -12,14 +12,24 @@
             <div class="first-title">
               <p>🏡 Địa chỉ</p>
             </div>
-              <div class="address-button">
-                <b-button type="is-success">➕ Thêm địa chỉ mới</b-button>
+            <div class="address-button">
+              <b-button type="is-success">➕ Thêm địa chỉ mới</b-button>
+            </div>
+          </div>
+          <div class="address-information">
+            <div class="columns">
+              <div class="column">
+                <p class="address-title"
+                 v-bind:address="address"
+            v-for="address in address"
+            v-bind:key="address.id">{{address.name}}</p>
               </div>
             </div>
           </div>
         </div>
       </div>
     </div>
+  </div>
 </template>
 <script>
 import TabInformation from "../components/UserInformation/TabInformation.vue";
@@ -31,6 +41,30 @@ export default {
     TabInformation,
     UserFirstDescription,
     UserInformationMenu
+  },
+  data() {
+    return {
+      address: [
+        {
+          id : 1,
+          name: "Bùi Trần Việt Anh",
+          phonenumber: "0912345678",
+          title: "Trang trại Thiên Thạch, ấp 6 thôn Đồng Phân",
+          firstaddress: "Xã Cù Mần",
+          secondadresss: "Huyện Trà Vinh",
+          city: "Thanh Hóa"
+        },
+        {
+          id : 1,
+          name: "Nguyễn Hoàng Minh Phương",
+          phonenumber: "0732569841",
+          title: "Trang trại Lếu Lều, đòn 7 ấp 2 thôn Quỳnh Hương",
+          firstaddress: "Xã Địa Đàng",
+          secondadresss: "Huyện Củ Chiêu",
+          city: "Thành phố Hồ Chí Minh"
+        }
+      ]
+    };
   }
 };
 </script>
@@ -62,8 +96,16 @@ export default {
   color: #07d390;
 }
 .content-middle {
-    display: flex;
-    justify-content: space-between;
-    width: 100%;
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+}
+.column {
+  height: 292px;
+  width: 505px;
+  border: 1px solid #00000015;
+  box-shadow: 0 2px 6px #00000014;
+  border-radius: 10px;
+  transition: 0.25s;
 }
 </style>
